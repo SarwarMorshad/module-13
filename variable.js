1afba5d6fade58ee11119d267bfbb6d31cc2dbbb
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-27 22:13:34 +0200

    Practice

---
+++ variable.js
@@ -0,0 +1,14 @@
+/*
+!!variable > 5 things to declare variables in JavaScript
+* 1. variable name
+* 2. variable value
+* 3. variable type
+*/
+
+var year = 2023; // variable name, value, type (number)
+let name = "John"; // variable name, value, type (string)
+const pi = 3.14; // variable name, value, type (number)
+
+var age; // variable name, type (undefined)
+
+console.log(age);
