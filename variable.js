cc2e24aa3dd410b6c0375cf7991272dfb1433cf2
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-27 22:56:10 +0200

    Practice

--- variable.js
+++ variable.js
@@ -11,4 +11,15 @@ const pi = 3.14; // variable name, value, type (number)
 
 var age; // variable name, type (undefined)
 
-console.log(age);
+// console.log(age);
+
+/*
+!!variable > Naming conventions
+* 1. No keywords (e.g., var, let, const, function, etc.)
+* 2. No spaces (e.g., my variable)
+* 3. No special characters (e.g., @, #, $, %, etc.)
+* 4. Name is case-sensitive (e.g., myVar vs myvar)
+* 5. Start with a letter or underscore (e.g., _myVar, myVar)
+*    - Avoid starting with a number (e.g., 1myVar is invalid)
+* 6. Use snake_Case/camelCase for multi-word names (e.g., my_variable, myVariable)
+*/
